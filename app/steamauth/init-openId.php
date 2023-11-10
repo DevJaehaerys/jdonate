ee3f2fc202bcc28e95c8b8577c28a68e1879e33b
Jaehaerys <130801761+DevJaehaerys@users.noreply.github.com>
2023-11-10 04:40:17 +0300

    fixed cloudflare flexible openid ssl conflict :^

--- app/steamauth/init-openId.php
+++ app/steamauth/init-openId.php
@@ -1,16 +1,20 @@
 <?php
 ob_start();
 session_start();
+
+$cffix = (!empty($_SERVER['HTTP_X_FORWARDED_PROTO'])) ? $_SERVER['HTTP_X_FORWARDED_PROTO'] . '://' : ((!empty($_SERVER['HTTPS'])) ? 'https://' : 'http://');
+
 $login_url_params = [
     'openid.ns'         => 'http://specs.openid.net/auth/2.0',
     'openid.mode'       => 'checkid_setup',
     'openid.return_to'  => 'https://free.jaehaerys.dev/app/steamauth/process-openId.php',
-    'openid.realm'      => (!empty($_SERVER['HTTPS']) ? 'https' : 'http').'://'.$_SERVER['HTTP_HOST'],
+    'openid.realm'      => $cffix . $_SERVER['HTTP_HOST'],
     'openid.identity'   => 'http://specs.openid.net/auth/2.0/identifier_select',
     'openid.claimed_id' => 'http://specs.openid.net/auth/2.0/identifier_select',
 ];
 
-$steam_login_url = 'https://steamcommunity.com/openid/login'.'?'.http_build_query($login_url_params, '', '&');
+$steam_login_url = 'https://steamcommunity.com/openid/login' . '?' . http_build_query($login_url_params, '', '&');
 
 header("location: $steam_login_url");
 exit();
+?>
