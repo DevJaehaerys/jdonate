c7497eb950e479c4abc7f24a7c780597adf13f66
Jaehaerys <130801761+DevJaehaerys@users.noreply.github.com>
2023-11-14 23:23:45 +0300

    fixed ability sql injection though payments data, special thx rostov114

--- app/payments/freekassa/red.php
+++ app/payments/freekassa/red.php
@@ -1,7 +1,7 @@
 <?php
-
+session_start();
 $order_amount = $_POST['amount'];
-$user = $_POST['steamid'];
+$user = htmlspecialchars($_SESSION['userData']['steam_id'] ?? '', ENT_QUOTES, 'UTF-8');
 if (is_numeric($order_amount) && floatval($order_amount) >= 10) {
     $merchant_id = ''; // ID мерчанта
     $secret_word = ''; // секретный ключ
@@ -12,6 +12,7 @@ if (is_numeric($order_amount) && floatval($order_amount) >= 10) {
     $sign = md5($merchant_id . ':' . $order_amount . ':' . $secret_word . ':' . $currency . ':' . $order_id);
     $url = 'https://pay.freekassa.ru/?m=' . $merchant_id . '&oa=' . $order_amount . '&o=' . $order_id . '&s=' . $sign . '&currency=RUB';
 
+    // Return the URL as JSON response
     echo json_encode(['redirectUrl' => $url]);
     exit;
 } else {
