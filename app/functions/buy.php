a48ba3238d28483da3d2c2b6bb836f83adf4753a
Jaehaerys <130801761+DevJaehaerys@users.noreply.github.com>
2023-11-15 17:03:54 +0300

    Update buy.php

--- app/functions/buy.php
+++ app/functions/buy.php
@@ -2,67 +2,35 @@
 ob_start();
 session_start();
 
-require_once './app/maim/User.php'; 
+require_once '../main/User.php';
 
 $user = new User();
+$itemId = isset($_POST['itemId']) ? $_POST['itemId'] : '';
+if (!ctype_digit($itemId)) {
+    return;
+}
 
 if ($user->checkAuthorization()) {
     $config = include('../main/config.php');
 
-    $itemId = mysqli_real_escape_string($user->getConnection(), $_POST['itemId']);
-    $steamid = mysqli_real_escape_string($user->getConnection(), $_SESSION['userData']['steam_id']);
+    $itemId = $_POST['itemId'];
+    $steamid = $_SESSION['userData']['steam_id'];
     $balance = $user->getUserBalance($steamid);
 
-    $balanceQuery = "SELECT balance FROM users WHERE steamid = '$steamid'";
-    $balanceResult = mysqli_query($user->getConnection(), $balanceQuery);
-    if (!$balanceResult) {
-        die("db error");
-    }
-
-    $balanceRow = mysqli_fetch_assoc($balanceResult);
-    $balance = $balanceRow['balance'];
-
-    $itemQuery = "SELECT price FROM shops WHERE id = '$itemId'";
-    $itemResult = mysqli_query($user->getConnection(), $itemQuery);
-    if (!$itemResult) {
-        die("db error");
-    }
-    $itemRow = mysqli_fetch_assoc($itemResult);
-    $itemPrice = $itemRow['price'];
+    $itemPrice = $user->getItemPrice($itemId);
 
     if ($balance >= $itemPrice) {
         $newBalance = $balance - $itemPrice;
         $user->updateBalance($steamid, $newBalance);
 
-        $commandQuery = "SELECT command, name, image FROM shops WHERE id = ?";
-        $stmt = $user->getConnection()->prepare($commandQuery);
-        $stmt->bind_param("i", $itemId);
-        $stmt->execute();
-        $commandResult = $stmt->get_result();
-        $stmt->close();
-
-        if (!$commandResult) {
-            die("db error");
-        }
-
-        $commandRow = $commandResult->fetch_assoc();
-        $command = $commandRow['command'];
-        $itemName = $commandRow['name'];
-        $itemImage = $commandRow['image'];
-        $command = str_replace("%player%", $steamid, $command);
+        $commandInfo = $user->getCommandInfo($itemId);
+        $command = $commandInfo['command'];
+        $itemName = $commandInfo['name'];
+        $itemImage = $commandInfo['image'];
         $commands = explode(",", $command);
         $commandsString = implode(", ", $commands);
 
-        $sql = "INSERT INTO cart (steamid, name, command, image) VALUES ('$steamid', '$itemName', '$commandsString', '$itemImage')";
-        if (!$user->getConnection()) {
-            die("db error");
-        }
-
-        if ($user->getConnection()->query($sql) === true) {
-            $response = array("success" => 'success');
-        } else {
-            die("db error");
-        }
+        $user->addToCart($steamid, $itemName, $commandsString, $itemImage);
 
         $response = array("success" => 'Успех блин!');
         echo json_encode($response);
@@ -74,6 +42,4 @@ if ($user->checkAuthorization()) {
     $response = array("auth" => 'Вы не авторизованы');
     echo json_encode($response);
 }
-
-$user->closeConnection(); 
 ?>
