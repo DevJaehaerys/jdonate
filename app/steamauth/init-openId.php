cb91c3c888ef2d9f18ef821b0ac12a148cbe6249
Jaehaerys <130801761+DevJaehaerys@users.noreply.github.com>
2023-11-16 16:37:25 +0300

    Update init-openId.php

--- app/steamauth/init-openId.php
+++ app/steamauth/init-openId.php
@@ -2,12 +2,14 @@
 ob_start();
 session_start();
 
-$cffix = (!empty($_SERVER['HTTP_X_FORWARDED_PROTO'])) ? $_SERVER['HTTP_X_FORWARDED_PROTO'] . '://' : ((!empty($_SERVER['HTTPS'])) ? 'https://' : 'http://');
+$protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http';
+$domain = $_SERVER['HTTP_HOST'];
+$cffix = !empty($_SERVER['HTTP_X_FORWARDED_PROTO']) ? $_SERVER['HTTP_X_FORWARDED_PROTO'] . '://' : "$protocol://";
 
 $login_url_params = [
     'openid.ns'         => 'http://specs.openid.net/auth/2.0',
     'openid.mode'       => 'checkid_setup',
-    'openid.return_to'  => 'https://free.jaehaerys.dev/app/steamauth/process-openId.php',
+    'openid.return_to'  => "$protocol://$domain/app/steamauth/process-openId.php",
     'openid.realm'      => $cffix . $_SERVER['HTTP_HOST'],
     'openid.identity'   => 'http://specs.openid.net/auth/2.0/identifier_select',
     'openid.claimed_id' => 'http://specs.openid.net/auth/2.0/identifier_select',
